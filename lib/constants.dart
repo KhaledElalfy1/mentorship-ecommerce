88fd74a7ad70d1fbb21923084b4b3f00679d8620
Bahnas <bahnas114@gmail.com>
2025-02-07 01:25:51 +0200

    styles and font done

---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+const kProductSans = 'Product Sans';
+
--- lib/ecommerce_app.dart
+++ lib/ecommerce_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mentorship_ecommerce/constants.dart';
 import 'package:mentorship_ecommerce/core/routes/app_router.dart';
 import 'package:mentorship_ecommerce/core/routes/routes.dart';
 
@@ -9,6 +10,9 @@ class EcommerceApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily:kProductSans,
+      ),
       initialRoute: Routes.home,
       onGenerateRoute: AppRouter().generateRouter,
     );
