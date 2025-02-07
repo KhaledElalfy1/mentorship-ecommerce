8b973afbd45412957cbe1ca883b7bc56683b3c88
Bahnas <bahnas114@gmail.com>
2025-02-08 01:25:54 +0200

    creating widgets file in core/utils &&  custome app bar done

--- lib/core/utils/app_color.dart
+++ lib/core/utils/app_color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColor {
-    static const coolGray = Color(0xff777E90);
+  static const coolGray = Color(0xff777E90);
   static const gunmetalGray = Color(0xff1D1F22);
   static const charcoalBrown = Color(0xff33302E);
   static const buttonColor = Color(0xff343434);
