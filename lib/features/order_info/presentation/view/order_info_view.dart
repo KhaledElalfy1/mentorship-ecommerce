e04417b15760b7a686aeb818235df7f939df2e65
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-02-13 20:33:55 +0200

    add order info view and related components with new order icon

--- lib/features/order_info/presentation/view/order_info_view.dart
+++ lib/features/order_info/presentation/view/order_info_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mentorship_ecommerce/core/utils/styles.dart';
 import 'package:mentorship_ecommerce/core/utils/widgets/custom_back_button.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mentorship_ecommerce/features/order_info/presentation/view/widgets/order_amount_container.dart';
+import 'package:mentorship_ecommerce/features/order_info/presentation/view/widgets/order_details_container.dart';
+import 'package:mentorship_ecommerce/features/order_info/presentation/view/widgets/order_state_container.dart';
 
 class OrderInfoView extends StatelessWidget {
   const OrderInfoView({super.key});
@@ -17,11 +21,20 @@ class OrderInfoView extends StatelessWidget {
         centerTitle: true,
         leading: const CustomBackButton(),
       ),
-      body: ListView(
-        children: [
-          
-        ],
-      )
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+        child: ListView(
+          children: [
+            const OrderStateContainer(),
+            SizedBox(height: 20.h),
+            const OrderDetailsContainer(),
+            SizedBox(height: 40.h),
+            const OrderAmountContainer(),
+            
+          ],
+        ),
+      ),
     );
   }
 }
+
