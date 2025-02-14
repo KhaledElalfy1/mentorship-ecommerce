5ab79201ef2bb3a34f4141f0f3405dab5141ed45
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-02-14 14:58:44 +0200

    - add review SVG and add it to Assets class. - add submet review dialog. - finish order rate screen.

--- lib/features/order_rate/presentation/view/order_rate_view.dart
+++ lib/features/order_rate/presentation/view/order_rate_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mentorship_ecommerce/core/utils/widgets/custom_back_button.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mentorship_ecommerce/core/utils/widgets/custom_button.dart';
 import 'package:mentorship_ecommerce/features/order_rate/presentation/view/widgets/review_container.dart';
 import 'package:mentorship_ecommerce/features/order_rate/presentation/view/widgets/stars_section.dart';
+import 'package:mentorship_ecommerce/features/order_rate/presentation/view/widgets/submet_review_dialog.dart';
+
 class OrderRateView extends StatelessWidget {
   const OrderRateView({super.key});
 
@@ -16,12 +19,28 @@ class OrderRateView extends StatelessWidget {
       ),
       body: ListView(
         children: [
-           SizedBox(height: 40.h),
+          SizedBox(height: 40.h),
           const StarsSection(),
           SizedBox(height: 20.h),
           const ReviewContainer(),
+          SizedBox(height: 100.h),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: SizedBox(
+              height: 50.h,
+              child: CustomButton(
+                buttonText: 'Submit Review',
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) =>const SubmitReviewDialog());
+                },
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
