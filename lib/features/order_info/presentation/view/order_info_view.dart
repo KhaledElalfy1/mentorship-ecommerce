bff0aa68686ec58e20f97584a20346f1bd0789f3
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-02-14 13:51:01 +0200

    - add buttons section and extract it as widget. - finish order info view.

--- lib/features/order_info/presentation/view/order_info_view.dart
+++ lib/features/order_info/presentation/view/order_info_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mentorship_ecommerce/core/utils/styles.dart';
 import 'package:mentorship_ecommerce/core/utils/widgets/custom_back_button.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mentorship_ecommerce/features/order_info/presentation/view/widgets/order_action_buttons.dart';
 import 'package:mentorship_ecommerce/features/order_info/presentation/view/widgets/order_amount_container.dart';
 import 'package:mentorship_ecommerce/features/order_info/presentation/view/widgets/order_details_container.dart';
 import 'package:mentorship_ecommerce/features/order_info/presentation/view/widgets/order_state_container.dart';
@@ -25,16 +26,17 @@ class OrderInfoView extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 24.0.w),
         child: ListView(
           children: [
+            SizedBox(height: 40.h),
             const OrderStateContainer(),
             SizedBox(height: 20.h),
             const OrderDetailsContainer(),
             SizedBox(height: 40.h),
             const OrderAmountContainer(),
-            
+            SizedBox(height: 40.h),
+            const OrderActionButtons(),
           ],
         ),
       ),
     );
   }
 }
-
