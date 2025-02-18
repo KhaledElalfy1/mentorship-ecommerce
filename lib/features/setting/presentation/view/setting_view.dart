d9438fd482f4be1e1afbca762fa5d3d067c3387d
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-02-18 17:48:57 +0200

    finish setting view

--- lib/features/setting/presentation/view/setting_view.dart
+++ lib/features/setting/presentation/view/setting_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mentorship_ecommerce/core/utils/assets.dart';
+import 'package:mentorship_ecommerce/core/utils/widgets/custom_app_bar.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mentorship_ecommerce/features/setting/presentation/view/widgets/setting_body.dart';
 
 class SettingView extends StatelessWidget {
   const SettingView({super.key});
@@ -6,7 +11,20 @@ class SettingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomAppBar(
+              icon: SvgPicture.asset(Assets.homeIcon),
+              title: 'Setting',
+            ),
+            SizedBox(
+              height: 60.h,
+            ),
+            const SettingBody(),
+          ],
+        ),
+      ),
     );
   }
 }
