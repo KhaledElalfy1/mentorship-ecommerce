6c40947a110a0b810e5c3f1f89e505452b1025df
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-02-18 18:53:15 +0200

    separate discount widget

--- lib/features/search/presentation/view/widgets/discount_section.dart
+++ lib/features/search/presentation/view/widgets/discount_section.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mentorship_ecommerce/core/utils/app_color.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mentorship_ecommerce/features/search/presentation/view/widgets/custom_discount_widget.dart';
 
 class DiscountSection extends StatelessWidget {
   const DiscountSection({
@@ -37,36 +37,3 @@ class DiscountSection extends StatelessWidget {
   }
 }
 
-class CustomDiscountWidget extends StatelessWidget {
-  const CustomDiscountWidget({
-    super.key,
-    required this.isSelected,
-    required this.discount,
-    required this.onSelected,
-  });
-  final bool isSelected;
-  final String discount;
-  final Function(bool) onSelected;
-  @override
-  Widget build(BuildContext context) {
-    return FilterChip(
-      backgroundColor: AppColor.whiteColor,
-      color: const WidgetStatePropertyAll(Colors.white),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-        side: const BorderSide(
-          color: AppColor.gunmetalGray,
-        ),
-      ),
-      selected: isSelected,
-      selectedColor: AppColor.gunmetalGray,
-      onDeleted: () {},
-      deleteIcon: const Icon(
-        Icons.close,
-        color: AppColor.gunmetalGray,
-      ),
-      label: Text(discount),
-      onSelected: onSelected,
-    );
-  }
-}
