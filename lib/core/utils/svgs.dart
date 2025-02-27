b396e4827fa9102857397be7489bf66112789226
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-02-27 21:06:35 +0200

    make main svgs path as variable and use it across the class

--- lib/core/utils/svgs.dart
+++ lib/core/utils/svgs.dart
@@ -1,25 +1,25 @@
 class Svgs {
-  // TODO: Convert asset paths (assets/svgs/) into variables and use them.
+  static const String _svgPath = 'assets/svgs/';
 
-  static const String appBarIcon = 'assets/svgs/app_bar_icon.svg';
-  static const String menuIcon = 'assets/svgs/Menu_svg.svg';
-  static const String homeIcon = 'assets/svgs/home_icon.svg';
-  static const String bugIcon = 'assets/svgs/bug_icon.svg';
-  static const String searchIcon = 'assets/svgs/search_icon.svg';
-  static const String personIcon = 'assets/svgs/person_icon.svg';
-  static const String settingIcon = 'assets/svgs/Setting.svg';
-  static const String favoriteIcon = 'assets/svgs/Favorite_fill.svg';
-  static const String logoutIcon = 'assets/svgs/Logout.svg';
-  static const String rightArrowIcon = 'assets/svgs/right_arrow.svg';
-  static const String leftArrowIcon = 'assets/svgs/right_arrow.svg';
-  static const String bellIcon = 'assets/svgs/Bell_pin.svg';
-  static const String filter = 'assets/svgs/Filter.svg';
-  static const String trash = 'assets/svgs/Trash.svg';
-  static const String search = 'assets/svgs/search.svg';
-  static const String rightArrow = 'assets/svgs/right_arrow.svg';
-  static const String orderIcon = 'assets/svgs/order.svg';
-  static const String orderDone = 'assets/svgs/review_done.svg';
-  static const String about = 'assets/svgs/about.svg';
-  static const String language = 'assets/svgs/language.svg';
-  static const String termsOFUse = 'assets/svgs/terms_of_use.svg';
+  static const String appBarIcon = '${_svgPath}app_bar_icon.svg';
+  static const String menuIcon = '${_svgPath}Menu_svg.svg';
+  static const String homeIcon = '${_svgPath}home_icon.svg';
+  static const String bugIcon = '${_svgPath}bug_icon.svg';
+  static const String searchIcon = '${_svgPath}search_icon.svg';
+  static const String personIcon = '${_svgPath}person_icon.svg';
+  static const String settingIcon = '${_svgPath}Setting.svg';
+  static const String favoriteIcon = '${_svgPath}Favorite_fill.svg';
+  static const String logoutIcon = '${_svgPath}Logout.svg';
+  static const String rightArrowIcon = '${_svgPath}right_arrow.svg';
+  static const String leftArrowIcon = '${_svgPath}right_arrow.svg';
+  static const String bellIcon = '${_svgPath}Bell_pin.svg';
+  static const String filter = '${_svgPath}Filter.svg';
+  static const String trash = '${_svgPath}Trash.svg';
+  static const String search = '${_svgPath}search.svg';
+  static const String rightArrow = '${_svgPath}right_arrow.svg';
+  static const String orderIcon = '${_svgPath}order.svg';
+  static const String orderDone = '${_svgPath}review_done.svg';
+  static const String about = '${_svgPath}about.svg';
+  static const String language = '${_svgPath}language.svg';
+  static const String termsOFUse = '${_svgPath}terms_of_use.svg';
 }
