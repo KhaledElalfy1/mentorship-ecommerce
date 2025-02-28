aa09e75daa4f690ebe73f07b409f644bbb9f7edf
Bahnas <bahnas114@gmail.com>
2025-02-28 17:06:55 +0200

    refactor recommended section in home done move lists to home_list

--- lib/features/home/presentation/view/widgets/home_lists.dart
+++ lib/features/home/presentation/view/widgets/home_lists.dart
@@ -1,7 +1,21 @@
 import 'package:mentorship_ecommerce/core/utils/assets.dart';
+import 'package:mentorship_ecommerce/features/home/data/models/recommended_item_model.dart';
 
 const featuredProductlist = [
   Assets.productImage1,
   Assets.productImage2,
   Assets.productImage3,
 ];
+
+  const recommendedItemslist =  [
+    RecommendedItemModel(
+      imageUrl: Assets.recommendedImage1,
+      title: 'White fashion hoodie',
+      price: 39.99,
+    ),
+    RecommendedItemModel(
+      imageUrl: Assets.recommendedImage2,
+      title: 'Cotton T-shirt',
+      price: 30.99,
+    ),
+  ];
