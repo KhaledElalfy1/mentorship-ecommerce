3b52252b944738ad04982ba1e4fa22a47201d7b8
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-03-09 15:11:57 +0200

    - add firebase analytics package. - add tracing getter in main file.

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,15 +1,21 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mentorship_ecommerce/core/functions/init_firebase_and_analytics.dart';
 import 'firebase_options_dev.dart';
 import 'ecommerce_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+  await initFirebaseAndAnalytics(
     options: DefaultFirebaseOptions.currentPlatform,
+    isAnalyticsEnabled: false,
   );
-  runApp(DevicePreview(builder: (context) {
-    return const EcommerceApp();
-  }));
+
+  runApp(
+    DevicePreview(
+      builder: (context) {
+        return const EcommerceApp();
+      },
+    ),
+  );
 }
