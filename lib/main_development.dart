1db2cb203e904385ea698da1592561a3f7e68ad5
Heba <hebatrka@gmail.com>
2025-03-12 20:55:46 +0200

    lntegrated Firebase Crashlytics into the project and added the required library and made necessary modification to Android and ios files

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,14 +1,17 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options_dev.dart';
 import 'ecommerce_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   runApp(DevicePreview(builder: (context) {
     return const EcommerceApp();
   }));
