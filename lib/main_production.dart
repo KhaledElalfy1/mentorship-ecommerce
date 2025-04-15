220693aaeb6de8b3fa29c563255521dd302bfec1
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-04-15 08:20:51 +0200

    add google file service without sha

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:mentorship_ecommerce/core/functions/init_firebase_and_analytics.dart';
 import 'ecommerce_app.dart';
 import 'firebase_options_prod.dart';
-
+import 'package:mentorship_ecommerce/core/services/prefs.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initFirebaseAndAnalytics(
     options: DefaultFirebaseOptions.currentPlatform,
     isAnalyticsEnabled: true,
   );
-
+  await Prefs.init();
   runApp(const EcommerceApp());
 }
