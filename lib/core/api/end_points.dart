22d155fef25b7c01d0fc1587b9cd3d33456e0ea3
Bahnas <bahnas114@gmail.com>
2025-07-15 00:48:57 +0300

    adding json serailizable

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -1,12 +1,7 @@
-
 class EndPoints {
   // base url
   static String baseUrl = "https://api.escuelajs.co/api/v1";
-
-
+  static String recommendedProductsEndPoint({required int id}) => '$id/products';
 }
 
-class ApiKeys {
-
-
-}
+class ApiKeys {}
